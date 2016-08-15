ca4938d1a1bebe17379aebf0265bfe26adc1cc9e
NotRaia <NotRaia@users.noreply.github.com>
2016-08-14 22:19:22 -0700

    Add check for XY contest stats

---
+++ src/checks/xy-contest-stats.js
@@ -0,0 +1,13 @@
+const gameCheck = require('../helpers/gameId.js');
+
+module.exports = {
+  description: 'A Pokémon that has never been traded from XY cannot have contest stats',
+  filter (pkmn) {
+    return gameCheck.isFromXY(pkmn.otGameId);
+  },
+  test (pkmn) {
+    const hasContestStats = pkmn.contestStatCool | pkmn.contestStatBeauty | pkmn.contestStatCute | pkmn.contestStatSheen
+    | pkmn.contestStatSmart | pkmn.contestStatTough;
+    return pkmn.notOt || !hasContestStats;
+  }
+};

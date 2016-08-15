bfbca626e4ee59aa5e311c6e0255bf3a565f8cd4
NotRaia <NotRaia@users.noreply.github.com>
2016-08-14 22:20:46 -0700

    Add check for past-gen PID/EC

---
+++ src/checks/pid-ec.js
@@ -0,0 +1,11 @@
+const gameCheck = require('../helpers/gameId.js');
+
+module.exports = {
+  description: 'A Pokémon transferred from pre-gen 6 must have a matching PID and EC',
+  filter (pkmn) {
+    return gameCheck.originGen(pkmn.otGameId) < 6;
+  },
+  test (pkmn) {
+    return pkmn.encryptionConstant === pkmn.pid;
+  }
+};

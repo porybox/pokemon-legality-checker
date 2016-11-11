826250b4866e090b791891941d9e92ac383307af
Alistair McKinlay <github@10people.co.uk>
2016-11-11 10:53:16 +0000

    Add game IDs for gen 7

--- src/helpers/gameId.js
+++ src/helpers/gameId.js
@@ -2,7 +2,8 @@ const genMap = {
   1: 3, 2: 3, 3: 3, 4: 3, 5: 3, 15: 3, /* S, R, E, FR, LG, XD/Colo */
   7: 4, 8: 4, 10: 4, 11: 4, 12: 4, /* HG, SS, D, P, Pt */
   20: 5, 21: 5, 22: 5, 23: 5, /* W, B, W2, B2 */
-  24: 6, 25: 6, 26: 6, 27: 6 /* X, Y, OR, AS */
+  24: 6, 25: 6, 26: 6, 27: 6, /* X, Y, OR, AS */
+  30: 7, 31: 7 /* Sun, Moon */
 };
 
 exports.isFromXY = function (gameId) {
@@ -11,6 +12,10 @@ exports.isFromXY = function (gameId) {
 
 exports.isFromORAS = function (gameId) {
   return [26, 27].indexOf(gameId) > -1;
+};
+
+exports.isFromGen7 = function (gameId) {
+  return [30, 31].indexOf(gameId) > -1;
 };
 
 exports.originGen = function (gameId) {

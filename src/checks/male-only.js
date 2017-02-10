4a16975612d9fa90e22054e6b82296c1f4a6a587
Teddy Katz <teddy.katz@gmail.com>
2017-02-09 22:21:51 -0500

    Change the lint warnings to errors (#11)
    I think we're all too good at ignoring warnings.

--- src/checks/male-only.js
+++ src/checks/male-only.js
@@ -3,7 +3,7 @@ module.exports = {
   field: 'gender',
   filter (pkmn) {
     return [32, 33, 34, 106, 107, 128, 236, 237, 313, 381, 414, 475,
-    538, 539, 627, 628, 641, 642, 645].includes(pkmn.dexNo);
+      538, 539, 627, 628, 641, 642, 645].includes(pkmn.dexNo);
   },
   test (pkmn) {
     return pkmn.gender === 'M';
